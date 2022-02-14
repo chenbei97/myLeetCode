24aafa60573dd39e7e1a2896a2b8e70a4dcaf3fa
chenbei <66169263+chenbei97@users.noreply.github.com>
2022-02-14 15:43:12 +0800

    Update main.cpp

--- binaryTree/main.cpp
+++ binaryTree/main.cpp
@@ -22,20 +22,8 @@ int main()
     // _binaryTree_levelOrder_queue();
     //_binaryTree_levelOrder_deque();
 
-<<<<<<< HEAD
     // cout <<"binaryTree is symmetric as follows\n";
-    // _binaryTree_is_symmetric_recursion();
+    // _binaryTree_isSymmetric_recursion();
     // _binaryTree_isSymmetric_array(); 
-=======
-<<<<<<< HEAD
-    // cout <<"binaryTree is symmetric as follows\n";
-    // _binaryTree_is_symmetric_recursion();
-    // _binaryTree_isSymmetric_array(); 
-=======
-    cout <<"binaryTree is symmetric as follows\n";
-    _binaryTree_is_symmetric_recursion();
-    _binaryTree_isSymmetric_array(); // 暂时无法解决层次遍历有nullptr打印-1的机制
->>>>>>> 1298c0e932053a3e07ba5ab8516edffec18d376f
->>>>>>> 3828144b85670da346a2823e0171c7ef2ee35bdd
     return 0;
 }
