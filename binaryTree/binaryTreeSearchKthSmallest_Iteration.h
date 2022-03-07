71be6c3f5486e30bef607a7aaf4cf3c0d4b83335
chenbei97 <chenbei_electric@163.com>
2022-03-07 15:04:31 +0800

    update

--- binaryTree/binaryTreeSearchKthSmallest_Iteration.h
+++ binaryTree/binaryTreeSearchKthSmallest_Iteration.h
@@ -2,24 +2,51 @@
 #define binaryTreeSearchKthSmallestIteration_
 #include <vector>
 #include <iostream>
+#include <unordered_map>
 #include "TreeNode.h"
 using namespace std;
 class binaryTreeSearchKthSmallestIteration{
     public:
-        int kthSmallest(TreeNode* root, int k){
-
+        binaryTreeSearchKthSmallestIteration(TreeNode*root){
+            this->root = root;
+            count_node_num(this->root);// 把所有节点及其子节点的数目计算完毕存到subNodeNum
         }
+        int kthSmallest(int k){
+            TreeNode *node = this->root;
+            while (node){
+                int n = get_node_num(node->left); // 获取node的左子树节点数量
+                if (n+1<k){ // 加上node有n+1个节点,比k小,只能从右子树找
+                    node = node->right;
+                    k = k-(n+1); // 右子树只需要找到第[k-(n+1)]这么小的元素即可
+                }
+                else if (n+1==k) return node->val;//node就是第k小
+                else node = node->left; // 说明第k小在node.left的下方
+            }
+            return -1; // 如果k给的有问题默认返回-1
+        }
+    protected:
+        unordered_map<TreeNode*,int> subNodeNum;
+        TreeNode * root ;
+        int count_node_num(TreeNode *node){
+            if (!node) return 0;
+            subNodeNum[node] = count_node_num(node->left)+count_node_num(node->right)+1;
+            return subNodeNum[node];
+        }
+        int get_node_num(TreeNode*node){
+            if (!node) return 0;
+            return subNodeNum[node];
+        }
+
 };
 void _binaryTree_searchKthSmallest_iteration(){
-    binaryTreeSearchKthSmallestIteration solution;
     TreeNode *root = new TreeNode(5);
     root->left = new TreeNode(3);
     root->right = new TreeNode(6);
     root->left->left = new TreeNode(2);
     root->left->right = new TreeNode(4);
-    //root->left->right->left = new TreeNode(1);
     root->right->right = new TreeNode(7);
-    int val = solution.kthSmallest(root,1);
+    binaryTreeSearchKthSmallestIteration solution(root);
+    int val = solution.kthSmallest(1);
     cout<<"iteration: the smallest k element is "<<val<<endl;
 }
 #endif // !binaryTreeSearchKthSmallestIteration
--- binaryTree/binaryTreeSearchKthSmallest_Iteration.py
+++ binaryTree/binaryTreeSearchKthSmallest_Iteration.py
@@ -1,8 +1,32 @@
-from typing import Optional
 from TreeNode import TreeNode
 class binaryTreeSearchKthSmallestIteration:
-    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        pass
+    def __init__(self,root) -> None:
+        self.subNodeNum = {} # 字典存储,键是node的地址,值是它和它的子节点数量
+        self.root = root
+        self._count_node_num(root) # 把self.subNodeNum初始化,所有节点都计算了数目
+    def kthSmallest(self,k: int) -> int:
+        node = self.root
+        while node:
+            subNum = self._get_node_num(node.left) # 计算的是node.left的节点数
+            if subNum+1< k: #  subNum加上node有n+1个节点,比k小,只能从右子树找
+                node = node.right 
+                k = k-subNum-1 # node+node的左子树共subNum+1个节点,在右子树寻找时应该找以它为根节点第k-subNum-1小的元素
+            elif subNum == k-1 : # 说明第k小就是节点node.left
+                return node.val
+            else: node = node.left # 子节点数大于k-1,算上node＞k,说明第k小在左子树
+    def _count_node_num(self,node:TreeNode)->int: # 计算节点node的子节点数量
+        if not node: # 空节点返回0
+            return 0
+        self.subNodeNum[node] = self._count_node_num(node.left)+\
+            self._count_node_num(node.right)+1 # node也算1个节点
+        return self.subNodeNum[node]
+    def _get_node_num(self,node:TreeNode)->int: #获取指定节点node的子节点数量
+        return self.subNodeNum[node] if node else 0
+    def printNodeNum(self):
+        for key,value in self.subNodeNum.items():
+            print(f"key = {key.val} num = {value}",end=" => ")
+        print()
+
 if __name__ == '__main__':
     root = TreeNode(5)
     root.left = TreeNode(3)
@@ -10,5 +34,6 @@ if __name__ == '__main__':
     root.right.right = TreeNode(7)
     root.left.left = TreeNode(2)
     root.left.right = TreeNode(4)
-    solution = binaryTreeSearchKthSmallestIteration()
-    print("iteration: the smallest k element is ",solution.kthSmallest(root,6))
+    solution = binaryTreeSearchKthSmallestIteration(root)
+    solution.printNodeNum()
+    print("iteration: the smallest k element is ",solution.kthSmallest(5))
