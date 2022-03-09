c1dddcde9aefc1ac6022c7a812f81f466c8d0e08
chenbei97 <chenbei_electric@163.com>
2022-03-09 11:15:26 +0800

    update

--- linearArray/linearArrayNonRepeatingSubset_Recursion.h
+++ linearArray/linearArrayNonRepeatingSubset_Recursion.h
@@ -6,11 +6,21 @@ using namespace std;
 class linearArrayNonRepeatingSubsetRecursion{
     public:
         vector<vector<int>> subsets(vector<int>& nums){
-            vector<vector<int>> ans;
-            int n = nums.size();
+            dfs(0, nums);
             return ans;
         }
+        void dfs(int cur, vector<int>& nums) {
+        if (cur == nums.size()) { //cur进入下次递归之前,[0,cur-1]的位置状态是确定的
+            ans.push_back(t); // 所有状态确定好后就会记录答案返回
+            return;
+        }
+        t.push_back(nums[cur]); // 选择这个位置的元素
+        dfs(cur + 1, nums);// 最开始会1路到底,元素全部选择
+        t.pop_back(); // 从全1开始退栈,1个1个的放弃,放弃1个元素的时候可以放弃任意一个,都是1个可能
+        dfs(cur + 1, nums); // 故最终递归时间复杂度O(2^n)
+    }
     protected:
+        vector<vector<int>> ans;
         vector<int> t;
 
 
--- linearArray/linearArrayNonRepeatingSubset_Recursion.py
+++ linearArray/linearArrayNonRepeatingSubset_Recursion.py
@@ -2,7 +2,19 @@ from typing import List
 class linearArrayNonRepeatingSubsetRecursion:
     def subsets(self,nums:List)->List[List]:
         ans = []
-        
+        t = []
+        def loop(cur,nums):
+            if len(nums) == cur:
+                # print("h3=>",t)
+                ans.append(t.copy()) # 每次添加的是t的元素,用copy整个新地址,不然t的改变会影响ans
+                return
+            t.append(nums[cur])
+            # print("h1=>",t)
+            loop(cur+1,nums)
+            t.pop()
+            # print("h2=>",t)
+            loop(cur+1,nums)
+        loop(0,nums)
         return ans
 
 if __name__ == '__main__':
