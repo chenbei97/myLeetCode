3eda6795ea6643b60328472c097a807e5a213b83
chenbei97 <chenbei_electric@163.com>
2022-03-18 11:52:43 +0800

    update heapSort

--- coreAlgorithms/heapSort.h
+++ coreAlgorithms/heapSort.h
@@ -1,7 +1,7 @@
 /*** 
  * @Author: chenbei
  * @Date: 2022-03-17 14:55:02
- * @LastEditTime: 2022-03-17 17:00:11
+ * @LastEditTime: 2022-03-18 11:52:23
  * @Description: heapSort.h
  * @FilePath: \myLeetCode\coreAlgorithms\heapSort.h
  * @A boy without dreams
@@ -18,7 +18,18 @@ template<typename T>
 void initialHeap(vector<T>&nums,bool reverse = false);
 template<typename T>
 T topHeap(vector<T>&nums);
-
+/*
+从最后一个非叶子结点遍历,是为了从最小的子树开始检查,最小子树只需要比较根节点和2个子节点就可以确定最小子树是否满足定义
+不满足定义,只需要交换1次,因为子节点不会再有子树会比根节点的值大,否则它不是最小子树
+然后继续检查倒数第2个子树,倒数第3个子树直到全局最大树,每个子树都是更上一级子树的子节点
+倒序检查的目的是若低级子树满足根堆定义,则高级子树的根节点值大于2个低级子树时那么高级树也必定满足根堆定义,因为低级子树已检查过定义
+但更大树如果存在子树(左或右子树)大于更大树的根节点值,很可能子树的子树也会比更大树的根节点值大
+这也是为何for循环内部的while循环要检查子树的所有子树,这是为了实现自上而下的交换,把小的值下移到子树,大的值上移到根
+while循环是自上而下的检查,每一轮都会把子树较大的移到根,最后一轮循环结束,那个节点把自己的值给了根,节点自己则应该接收更大树根值
+单纯的交换根节点和某个子树的子树的..子树的某个节点,虽然保证了当前根节点大于了所有子节点
+但是可能会破坏某个子树的子树的..子树的根节点也要大于它下方的所有节点的事实,因为所有子树都要满足根节点大于子节点
+而倒序遍历因为是从最小子树往全局最大树检查的,基层子树开始就保证不会破坏这种关系,那么全局树也不会破坏这种关系
+*/
 template<typename T>
 void heapSort(vector<T>&nums, bool reverse = false){
     vector<int> ans;
@@ -45,36 +56,49 @@ void initialHeap(vector<T>&nums,bool reverse){
                 ++child; // 如果兄弟节点更大,只需要比较根节点和兄弟节点即可
             if (reverse?rootVal>=nums[child]:rootVal<=nums[child])
                 break; // 根节点值比最大的子节点那个还大,说明root以下的子树满足大根堆定义
-            // 不满足大根堆的定义时nums[child]<nums[root]
-            // 把子节点的值和根节点的值交换,就符合根节点>子节点的定义了
+            
+            // 不满足大根堆的定义时nums[child]<nums[root],但是有可能child的child也会比root大
+            // 除了交换root和child,在child不超出范围时也要继续检测child的子树
             nums[child/2] = nums[child]; // root就是child/2,<=>nums[root]=nums[child]
-            child *= 2; // 由于root会慢慢向全局根节点移动,child的孩子2*child和2*child+1
+            child *= 2; // 检测child的孩子2*child和2*child+1,下一轮while进行判断,必要的话也会交换child和它的child
             // 有可能会nums[child*2]或nums[child*2+1]>nums[child]
-            // 让child*2和child*2+1的较大者和child比较
-            // 那么nums[child]=max(nums[child*2],nums[child*2+1])
+            // 让child*2和child*2+1的较大者和child比较,child大则已,小就交换
+            // 因为child在进入下一轮while之前已经变成child's child,所以依然nums[child/2]=max(nums[child],nums[child+1])
             // 直到child超出n-1的数组范围
         }
-        // 在while内,根节点会被子树内最合适(大)的节点依次向上替换
+        // 在while内,如果不满足根堆定义,每个根节点都会和其孩子的较大值交换,最后1次while结束child是孩子
+        // 它的父亲child/2将其父亲child/4替换了,child/2本身应当接收原本较小的那个根节点值也就是rootVal
         // 根节点也应当回到子树内这个合适的节点位置,while结束后child/2就是这个位置
         nums[child/2] = rootVal; // 最合适的那个节点接收原来的根节点值
     }
-
 }
+/*
+本质上不是删除堆顶元素,而是用数组后边的元素去覆盖前边的元素
+事实上取出的是数组的最后1个元素,这样方便删除,然后把这个元素插入到合适的位置即可
+这里的while循环是自上而下的检查,而不是初始化时自下而上的检查
+在while内部尝试找到某个合适的位置插入这个元素,如果插入的元素比2个子树的节点值都大,就把元素覆盖到2个子树的根节点
+因为是自顶向下的,元素能比2个子树节点都大的话,说明就找到了合适的位置,2个子树的下方必定不会有更合适的位置
+然后把原本不合适的2个子树根节点的值挪到后边,它将在以后的for循环中被检查
+如此重复,假设能检查到最后1个非叶子节点,它一定是要插入元素的根节点,它如果没有插入的元素更合适,就让元素替换这个非叶子节点
+这样其实相当于要插入的元素还是最大/小的,它只是覆盖到前一个位置
+*/
 template<typename T>
 void popHeap(vector<T>&nums,bool reverse){
+    // 移除全局根节点,然后重新排列根堆,就是用后面的覆盖前边的即可
     int lastIdx = nums.size()-1;
-    T lastVal = nums[lastIdx--];
+    T lastVal = nums[lastIdx--];//所以记录好最后1个元素
     nums.pop_back();
-    int root=0,child=1;
-    while (child<=lastIdx){
+    int child=1; // 覆盖的过程和初始化根堆过程是一样的,root是现在的nums[0],区别是自上而下的遍历
+    while (child<=lastIdx){ // 类似于检查子树是否满足根堆定义,重构根堆
         if (child<lastIdx && (reverse?nums[child]<nums[child+1]:nums[child]>nums[child+1]))
-            ++child;
+            ++child; // 找到2个更大/小的子节点和要插入的节点比较
         if (reverse?lastVal>=nums[child]:lastVal<=nums[child])
-            break;
-        nums[child/2] = nums[child];
-        child *= 2; 
+            break; // 要插入的节点大/小于2个子节点就一定满足根堆定义
+        nums[child/2] = nums[child]; // 把大的子节点值覆盖根节点root
+        child *= 2; // 不满足,插入节点更合适的位置应当在child的child中寻找
     }
-    nums[child/2] = lastVal; 
+    // 每1轮while结束,child都是提前扩到2倍,child/2才是最后1个换到其根节点child/4的节点
+    nums[child/2] = lastVal; // 最后1轮,child/2换到child/4,故child/2是最适合lastVal插入的位置
 }
 template<typename T>
 T topHeap(vector<T>&nums){
--- coreAlgorithms/heapSort.py
+++ coreAlgorithms/heapSort.py
@@ -1,25 +1,69 @@
 '''
 Author: chenbei
 Date: 2022-03-17 14:56:27
-LastEditTime: 2022-03-17 14:57:43
+LastEditTime: 2022-03-18 11:25:43
 Description: heapSort.py
 FilePath: \myLeetCode\coreAlgorithms\heapSort.py
 A boy without dreams
 '''
-from typing import List,Any
+from typing import List
 class heapSort:
     def __init__(self,nums:List[int or float],reverse=False) -> None:
         self.nums = nums
+        self.n = len(self.nums)
         self.reverse = reverse
-    def heap_sort(self)->Any:
-        pass
+    def heap_sort(self)->None:
+        self.tnums = self.nums.copy() # 写在这里是避免二次调用heap_sort时出错,深拷贝改变tnums不会改变nums
+        ans = []
+        self.initialHeap()
+        while len(self.tnums)>0:
+            ans.append(self.topHeap())
+            self.popHeap()
+        for x in range(self.n): # 不要用self.nums = ans,会改变原有地址
+            self.nums[x] = ans[x] # 赋值不改变最初传入的nums地址
+    def initialHeap(self)->None:
+        for root in reversed(range(self.n//2+1)):
+            rootVal = self.tnums[root]
+            child = root * 2
+            while child<=self.n-1: # 检查root的所有子节点
+                if child<self.n-1 and (self.tnums[child]<self.tnums[child+1] \
+                    if self.reverse else self.tnums[child]>self.tnums[child+1]):
+                        child += 1 # 找到2个子节点更大的那个和根节点比较
+                if rootVal>=self.tnums[child] if self.reverse else rootVal<=self.tnums[child]:
+                    break # 如果大根堆时根节点大于子节点或者小根堆时根节点小于子节点则必定root也满足根堆定义
+                self.tnums[child//2] = self.tnums[child] # 否则根节点被子节点覆盖
+                child *= 2 # 继续检查后边的子节点,很可能child的child也会比root大
+            # while结束后,child多乘了1次2,所以while内执行的是child//2覆盖child//4的值
+            self.tnums[child//2] = rootVal # child/2应当被之前换下来的较小值rootVal覆盖
+        # print(self.tnums) # 9 5 3 4 1 0 2
+    def popHeap(self)->None:
+        lastIdx = len(self.tnums)-1
+        lastVal = self.tnums[lastIdx] # 把根堆最大/小值移除后就是把后面的值依次往前覆盖
+        self.tnums.pop(-1)
+        lastIdx  = lastIdx-1
+        child = 1 # 覆盖的过程和初始化根堆过程是一样的,root是现在的nums[0],区别是自上而下的遍历
+        while child<=lastIdx:
+            if child<lastIdx and (self.tnums[child]<self.tnums[child+1] \
+                if self.reverse else self.tnums[child]>self.tnums[child+1]):
+                    child += 1 # 找到2个更大/小的子节点和要插入的节点比较       
+            if lastVal>=self.tnums[child] if self.reverse else lastVal<=self.tnums[child]:
+                break; # 要插入的节点大/小于2个子节点就一定满足根堆定义
+            self.tnums[child//2] = self.tnums[child]; # 把大/小的子节点值覆盖根节点root
+            child *= 2; # 不满足,插入节点更合适的位置应当在child的child中寻找
+        # 每1轮while结束,child都是提前扩到2倍,child/2才是最后1个换到其根节点child/4的节点
+        if self.tnums == []:return # 防止出现 list assignment index out of range 错误
+        self.tnums[child//2] = lastVal # 最后1轮,child/2换到child/4,故child/2是最适合lastVal插入的位置
+
+    def topHeap(self)->int or float:
+        return self.tnums[0]
+
     def setReverse(self,reverse=False)->None:
         self.reverse = reverse
 if __name__ == '__main__':
-    nums = [2,6,10,4,8,12,-1,3]
-    solution = heapSort(nums,False)
-    solution.heap_sort(0,7)
+    nums = [5,4,3,2,1,0,9] # 2,6,10,4,8,12,-1,3
+    solution = heapSort(nums,True)
+    solution.heap_sort()
     print(nums)
-    solution.setReverse(True)
-    solution.heap_sort(0,7)
+    solution.setReverse(False)
+    solution.heap_sort()
     print(nums)
