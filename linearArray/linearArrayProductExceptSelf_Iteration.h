8ec10eb354a0c31677c1f1e7d3a0219ec1309708
chenbei97 <chenbei_electric@163.com>
2022-04-01 08:06:32 +0800

    product except self by iteration

--- linearArray/linearArrayProductExceptSelf_Iteration.h
+++ linearArray/linearArrayProductExceptSelf_Iteration.h
@@ -1,7 +1,7 @@
 /*** 
  * @Author: chenbei
  * @Date: 2022-03-31 16:31:58
- * @LastEditTime: 2022-03-31 16:59:31
+ * @LastEditTime: 2022-04-01 08:04:14
  * @Description: linearArrayProductExceptSelf_Iteration.h
  * @FilePath: \myLeetCode\linearArray\linearArrayProductExceptSelf_Iteration.h
  * @A boy without dreams
@@ -15,17 +15,26 @@ using namespace std;
 class linearArrayProductExceptSelfIteration{
     public:
         vector<int> roductExceptSelf(vector<int>&nums){
-            // 创建两个数组,一个用于记录某元素前元素的乘积
-            // 一个用于记录某元素后元素的乘积,最后用两个元素进行逐元素相乘即可
+            // 创建两个数组,一个用于记录某元素前所有元素的乘积
+            // 一个用于记录某元素后所有元素的乘积
+            // 最后两个数组的元素进行逐元素相乘即可其实就得到nums每个元素以外的所有元素乘积
+            /*
                 ostream_iterator<int> os(cout," ");
                 int n = nums.size();
                 vector<int> left(n, 1); // 初始化为1是为了方便相乘
                 vector<int> right(n, 1);
                 vector<int> res(n, 1);
-                for(int i = 1; i < nums.size(); i++) 
-                    left[i] = left[i-1] * nums[i-1]; 
-                for(int j = nums.size()-2; j>=0; j--)
-                    right[j] = right[j+1] * nums[j+1];//
+                // for(int i = 1; i < nums.size(); i++) 
+                //     //从left[i]i≥1,开始总是存储nums[i]之前的元素乘积,i=1开始,left[0]=1已初始化
+                //     left[i] = left[i-1] * nums[i-1]; 
+                // for(int j = nums.size()-2; j>=0; j--)
+                //     //从right[j]j<=n-2,开始总是存储nums[j]之后的元素乘积,j=n-2开始,left[n-1]=1已初始化
+                //     right[j] = right[j+1] * nums[j+1];
+                for(int i =1;i<nums.size();++i){
+                    // 上述2个for循环写成1个也对
+                    left[i] = left[i-1] * nums[i-1];
+                    right[n-1-i] = right[n-i] * nums[n-i]; 
+                }
                 for(int i = 0; i<nums.size(); i++)
                     res[i] = left[i] * right[i];
                 copy(left.begin(),left.end(),os);
@@ -33,19 +42,28 @@ class linearArrayProductExceptSelfIteration{
                 copy(right.begin(),right.end(),os);
                 cout<<"\n";// 24 12 4 1 = [right[3]*nums[3],right[2]*nums[2],right[3]*nums[3],1]
                 return res;
+            */
             
-            // int n = nums.size();
-            // vector<int> ans(n,1);
-            // int left=1,right=1;
-            // for(int i=0;i<n;++i){
-            //     ans[i] = left;
-            //     left *= nums[i];
+            // 优化的写法,不再使用2个数组,只需要2个数动态的等于nums[i]之前和之后的乘积即可
+            int n = nums.size();
+            vector<int> ans(n);
+            int left=1,right=1;
+            for(int i=0;i<n;++i){//ans先存储每个nums[i]的左边元素乘积
+                ans[i] = left; // ans[0]=1,left初始化为1的原因,left计算完用于下一次的left计算,即left=left*nums[0]
+                left *= nums[i];//left是nums[0]..nums[n-1]的动态乘积,nums[0]用于下一次的left计算
+            }
+            for (int i=n-1;i>=0;--i){ //ans再依次和右边元素的乘积相乘计算,但是倒序计算
+                ans[i] *= right;// ans[n-1]=1,因为nums[n-1]右边没有元素,right初始化为1的意义
+                right *= nums[i]; // right是nums[n-1]...nums[0]的动态乘积,nums[n-1]是用于right=nums[n-1]*right的下一次计算
+            }
+            // 也是合并2个for的写法
+            // for(int i =1;i<nums.size();++i){
+            //     left *= nums[i-1]; // left取nums[0]...nums[n-1]的动态乘积
+            //     right *= nums[nums.size()-i];//right取nums[n-1]...nums[1]的动态乘积
+            //     ans[i] *= left; // 同时更新ans的头部和尾部,等left到右边时右边已恭候多时ans[left]早就是那个元素后边的乘积了,left是左边元素乘积
+            //     ans[nums.size()-1-i] *= right;// right到左边时,ans[right]早就存储了元素之前的乘积,right是该元素右边的乘积
             // }
-            // for (int i=n-1;i>=0;--i){
-            //     ans[i] *= right;
-            //     right *= nums[i];
-            // }
-            // return ans;
+            return ans;
         }
 };
 void _linearArray_productExceptSelf_iteration(){
--- linearArray/linearArrayProductExceptSelf_Iteration.py
+++ linearArray/linearArrayProductExceptSelf_Iteration.py
@@ -9,15 +9,21 @@ A boy without dreams
 from typing import List
 class linearArrayProductExceptSelfIteration:
     def productExceptSelf(self,nums:List[int])->List[int]:
+        # 双数组的写法不在给出,参考c++的写法
         n = len(nums)
         ans = [1 for _ in range(n)]
         left = right = 1
-        for i in range(n):
-            ans[i] = left
-            left *= nums[i]
-        for i in range(n - 1, -1, -1):
-            ans[i] *= right
-            right *= nums[i]
+        # for i in range(n): # ans先存储每个nums[i]的左边元素乘积
+        #     ans[i] = left # ans[0]=1,left初始化为1的原因,left计算完用于下一次的left计算,即left=left*nums[0]
+        #     left *= nums[i] # left是nums[0]..nums[n-1]的动态乘积,nums[0]用于下一次的left计算
+        # for i in range(n - 1, -1, -1):# ans再依次和右边元素的乘积相乘计算,但是倒序计算
+        #     ans[i] *= right # ans[n-1]=1,因为nums[n-1]右边没有元素,right初始化为1的意义
+        #     right *= nums[i] # right是nums[n-1]...nums[0]的动态乘积,nums[n-1]是用于right=nums[n-1]*right的下一次计算
+        for i in range(1,n):
+            left *= nums[i-1] # left取nums[0]...nums[n-1]的动态乘积
+            right *= nums[n-i] # right取nums[n-1]...nums[1]的动态乘积
+            ans[i] *= left # 同时更新ans的头部和尾部,等left到右边时右边已恭候多时ans[left]早就是那个元素后边的乘积了,left是左边元素乘积
+            ans[n-1-i] *= right # right到左边时,ans[right]早就存储了元素之前的乘积,right是该元素右边的乘积
         return ans
 if __name__ == '__main__':
     nums = [1,2,3,4]
