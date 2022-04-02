98c44a794f9983bf86f905c25d26e1c5e175f9d6
chenbei97 <chenbei_electric@163.com>
2022-04-02 16:19:53 +0800

    get odd even list

--- linkedList/linkedListOddEven_Iteration.h
+++ linkedList/linkedListOddEven_Iteration.h
@@ -1,7 +1,7 @@
 /*** 
  * @Author: chenbei
  * @Date: 2022-04-02 11:30:48
- * @LastEditTime: 2022-04-02 13:26:33
+ * @LastEditTime: 2022-04-02 16:17:14
  * @Description: linkedListOddEven_Iteration.h
  * @FilePath: \myLeetCode\linkedList\linkedListOddEven_Iteration.h
  * @A boy without dreams
@@ -13,18 +13,30 @@
 using namespace std;
 class linkedListOddEvenIteration{
     public:
-        ListNode* oddEven(ListNode * root){
-            ListNode* ans;
-            return  ans;
+        ListNode* oddEven(ListNode * head){
+        if (head == nullptr)return head;
+        ListNode* evenHead = head->next; //偶数链表起点
+        ListNode* odd = head;//奇数链表起点
+        ListNode* even = evenHead;//维护2个指针odd和even
+        while (even != nullptr && even->next != nullptr) { 
+            //因为even在后,它和even.next都不为空时odd才能移动到even.next
+            odd->next = even->next;//奇数节点的下一个在偶数节点的下一个
+            odd = odd->next;// 更新odd到下一个
+            even->next = odd->next;//偶数节点的下一个是已更新的odd的下一个
+            even = even->next;// 更新even
         }
+        // 此时odd是最后1个奇数节点
+        odd->next = evenHead;//把偶数链表接在后边
+        return head;
+    }
 };
 void _linkedList_oddEven_iteration(){
     linkedListOddEvenIteration solution;
-    ListNode *root = new ListNode (3);
+    ListNode *root = new ListNode (1);
     root->next = new ListNode(2);
-    root->next->next = new ListNode(0);
-    root->next->next->next = new ListNode(-4);
-    root->next->next->next->next = new ListNode(4);
+    root->next->next = new ListNode(3);
+    root->next->next->next = new ListNode(4);
+    root->next->next->next->next = new ListNode(5);
     auto ans = solution.oddEven(root);
     cout<<"get oddEven by iteration as follows\n";
     while (ans){
--- linkedList/linkedListOddEven_Iteration.py
+++ linkedList/linkedListOddEven_Iteration.py
@@ -9,17 +9,28 @@ A boy without dreams
 from ListNode import ListNode
 class linkedListOddEvenIteration:
     def oddEven(self,root:ListNode)->None:
-        pass
+        if not root : return root
+        odd = root
+        evenHead = root.next
+        even = evenHead
+        while even and even.next:
+            odd.next = even.next
+            odd = odd.next
+            even.next = odd.next
+            even = even.next
+        odd.next = evenHead
+        return root
+
 if __name__ == '__main__':
-    root = ListNode(10)
-    root.next = ListNode(9)
-    root.next.next = ListNode(6)
-    root.next.next.next = ListNode(8)
-    root.next.next.next.next = ListNode(4)
-    root.next.next.next.next.next = ListNode(5)
-    root.next.next.next.next.next.next = ListNode(2)
-    root.next.next.next.next.next.next.next = ListNode(7)
-    root.next.next.next.next.next.next.next.next = ListNode(1)
+    root = ListNode(1)
+    root.next = ListNode(2)
+    root.next.next = ListNode(3)
+    root.next.next.next = ListNode(4)
+    root.next.next.next.next = ListNode(5)
+    root.next.next.next.next.next = ListNode(6)
+    root.next.next.next.next.next.next = ListNode(7)
+    root.next.next.next.next.next.next.next = ListNode(8)
+    root.next.next.next.next.next.next.next.next = ListNode(9)
     solution = linkedListOddEvenIteration()
     ans = solution.oddEven(root)
     while ans:
