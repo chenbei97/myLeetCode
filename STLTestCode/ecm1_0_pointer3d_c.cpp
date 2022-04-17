15da12f5aab5c06d9fe370a64582f85e9e0dc6bb
chenbei97 <chenbei_electric@163.com>
2022-04-17 09:03:07 +0800

    ecm1.0_pointer3d_c.cpp

--- STLTestCode/ecm1_0_pointer3d_c.cpp
+++ STLTestCode/ecm1_0_pointer3d_c.cpp
@@ -1,13 +1,13 @@
 /*
  * @Author: chenbei
  * @Date: 2022-04-17 08:37:52
- * @LastEditTime: 2022-04-17 08:47:33
- * @FilePath: \myLeetCode\STLTestCode\ecm1.0_pointer3d_c.cpp
+ * @LastEditTime: 2022-04-17 09:02:44
+ * @FilePath: \myLeetCode\STLTestCode\ecm1_0_pointer3d_c.cpp
  * @Description: ecm1.0_pointer3d_c.cpp
  * @Signature: A boy without dreams
  */
 #include <cstdio>
-using namespace std;
+#define Point3DPrint( pd ) printf("(%g, %g, %g )",pd->x,pd->y,pd->z);
 typedef struct point3d{
         float x;
         float y;
@@ -20,7 +20,12 @@ int main(){
     pd->x = 1.0;
     pd->y = 2.0;
     pd->z = 3.0;
+
+    // 使用函数,也可以使用宏
+    printf("by function =>\n");
     print_pd(pd);
+    printf("by define =>\n");
+    Point3DPrint(pd);
     return 0;
 }
 void print_pd(const Point3D* pd){
