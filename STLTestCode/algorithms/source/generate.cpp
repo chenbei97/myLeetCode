96bef03c77039749f21289eca4fe05e2a05a5263
chenbei97 <chenbei_electric@163.com>
2022-04-24 14:56:38 +0800

    generate

--- STLTestCode/algorithms/source/generate.cpp
+++ STLTestCode/algorithms/source/generate.cpp
@@ -1,7 +1,7 @@
 /*** 
  * @Author: chenbei
  * @Date: 2022-04-21 11:16:40
- * @LastEditTime: 2022-04-21 11:23:27
+ * @LastEditTime: 2022-04-24 14:53:40
  * @Description: test generate.cpp
  * @FilePath: \myLeetCode\STLTestCode\algorithms\source\generate.cpp
  * @Signature: A boy without dreams
@@ -21,6 +21,10 @@ int main()
     vector<int> src(20);
     generate(src.begin(), src.end(), [](){return rand()%100;});
     ostream_iterator<int> out(cout," ");
+    copy(src.begin(), src.end(), out);
+    cout << endl;
+    int i = 0; // 用于制造升序序列
+    generate(src.begin(), src.end(), [&i](){i+=2; return i;});
     copy(src.begin(), src.end(), out);
     cout << endl;
     return 0;
