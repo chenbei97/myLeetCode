0be29b14683dcc5b97434d97def4653ec8394028
chenbei97 <chenbei_electric@163.com>
2022-06-12 18:21:15 +0800

    update 各类软件、环境配置、问题解决指南

---
+++ Configuration-Code-Software-Guide/vscode_testFiles/test_opencv_for_vscode.cpp
@@ -0,0 +1,29 @@
+/*** 
+ * @Author: chenbei
+ * @Date: 2022-04-15 09:35:37
+ * @LastEditTime: 2022-04-15 10:30:19
+ * @Description: opencv 
+ * @FilePath: \myLeetCode\myPleasure\myOpencv\main.cpp
+ * @Signature: A boy without dreams
+ */
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/xfeatures2d.hpp>
+using namespace std;
+using namespace cv;
+int main(){
+    cout<<"hello opencv"<<endl;
+    cv::Mat src = cv::imread("myBaby.jpg",IMREAD_GRAYSCALE);
+    if (!src.empty())
+    {
+        std::cout << "src.depth()=" << src.depth() << std::endl;
+        cv::namedWindow("input", WINDOW_FREERATIO);//可调的自由比例
+        cv::imshow("input", src);
+        cv::waitKey(0);
+        cv::destroyAllWindows();
+    }
+    else
+        printf("could not load image...");
+    return 0;
+}
