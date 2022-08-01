535513d4640371bd8f43a9d888c87254fb4694fd
chenbei97 <chenbei_electric@163.com>
2022-08-01 16:02:01 +0800

    TestUdpUnitMultiBroadCast 演示如何进行UDP通信(组播模式)

--- Qt5.9.9/32-TestUdpUnitMultiBroadCast/UdpMultiCast/UdpMultiCast.h
+++ Qt5.9.9/32-TestUdpUnitMultiBroadCast/UdpMultiCast/UdpMultiCast.h
@@ -2,6 +2,8 @@
 #define UDPMULTICAST_H
 
 #include <QMainWindow>
+#include <QUdpSocket>
+#include <QLabel>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class UdpMultiCast; }
@@ -10,11 +12,23 @@ QT_END_NAMESPACE
 class UdpMultiCast : public QMainWindow
 {
   Q_OBJECT
-
+private:
+  QLabel  *LabSocketState;
+  QUdpSocket  *udpSocket;//ud客户端
+  QHostAddress groupAddress;//组播地址
+  QString getLocalIP();//获取本机IP地址
 public:
-  UdpMultiCast(QWidget *parent = nullptr);
+  explicit UdpMultiCast(QWidget *parent = nullptr);
   ~UdpMultiCast();
-
+private slots://自定义槽函数
+  void onSocketStateChange(QAbstractSocket::SocketState socketState);
+  void onSocketReadyRead();
+private slots:
+  void on_actStart_triggered();
+  void on_actStop_triggered();
+  void on_actHostInfo_triggered();
+  void on_actClear_triggered();
+  void on_btnMulticast_clicked();
 private:
   Ui::UdpMultiCast *ui;
 };
--- Qt5.9.9/32-TestUdpUnitMultiBroadCast/build-UdpMultiCast-Desktop_Qt_5_9_9_MinGW_32bit-Debug/debug/moc_UdpMultiCast.cpp
+++ Qt5.9.9/32-TestUdpUnitMultiBroadCast/build-UdpMultiCast-Desktop_Qt_5_9_9_MinGW_32bit-Debug/debug/moc_UdpMultiCast.cpp
@@ -21,8 +21,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_UdpMultiCast_t {
-    QByteArrayData data[1];
-    char stringdata0[13];
+    QByteArrayData data[11];
+    char stringdata0[207];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -31,10 +31,24 @@ struct qt_meta_stringdata_UdpMultiCast_t {
     )
 static const qt_meta_stringdata_UdpMultiCast_t qt_meta_stringdata_UdpMultiCast = {
     {
-QT_MOC_LITERAL(0, 0, 12) // "UdpMultiCast"
+QT_MOC_LITERAL(0, 0, 12), // "UdpMultiCast"
+QT_MOC_LITERAL(1, 13, 19), // "onSocketStateChange"
+QT_MOC_LITERAL(2, 33, 0), // ""
+QT_MOC_LITERAL(3, 34, 28), // "QAbstractSocket::SocketState"
+QT_MOC_LITERAL(4, 63, 11), // "socketState"
+QT_MOC_LITERAL(5, 75, 17), // "onSocketReadyRead"
+QT_MOC_LITERAL(6, 93, 21), // "on_actStart_triggered"
+QT_MOC_LITERAL(7, 115, 20), // "on_actStop_triggered"
+QT_MOC_LITERAL(8, 136, 24), // "on_actHostInfo_triggered"
+QT_MOC_LITERAL(9, 161, 21), // "on_actClear_triggered"
+QT_MOC_LITERAL(10, 183, 23) // "on_btnMulticast_clicked"
 
     },
-    "UdpMultiCast"
+    "UdpMultiCast\0onSocketStateChange\0\0"
+    "QAbstractSocket::SocketState\0socketState\0"
+    "onSocketReadyRead\0on_actStart_triggered\0"
+    "on_actStop_triggered\0on_actHostInfo_triggered\0"
+    "on_actClear_triggered\0on_btnMulticast_clicked"
 };
 #undef QT_MOC_LITERAL
 
@@ -44,22 +58,61 @@ static const uint qt_meta_data_UdpMultiCast[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       0,    0, // methods
+       7,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
        0,       // signalCount
 
+ // slots: name, argc, parameters, tag, flags
+       1,    1,   49,    2, 0x08 /* Private */,
+       5,    0,   52,    2, 0x08 /* Private */,
+       6,    0,   53,    2, 0x08 /* Private */,
+       7,    0,   54,    2, 0x08 /* Private */,
+       8,    0,   55,    2, 0x08 /* Private */,
+       9,    0,   56,    2, 0x08 /* Private */,
+      10,    0,   57,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void, 0x80000000 | 3,    4,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
        0        // eod
 };
 
 void UdpMultiCast::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
-    Q_UNUSED(_o);
-    Q_UNUSED(_id);
-    Q_UNUSED(_c);
-    Q_UNUSED(_a);
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        UdpMultiCast *_t = static_cast<UdpMultiCast *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->onSocketStateChange((*reinterpret_cast< QAbstractSocket::SocketState(*)>(_a[1]))); break;
+        case 1: _t->onSocketReadyRead(); break;
+        case 2: _t->on_actStart_triggered(); break;
+        case 3: _t->on_actStop_triggered(); break;
+        case 4: _t->on_actHostInfo_triggered(); break;
+        case 5: _t->on_actClear_triggered(); break;
+        case 6: _t->on_btnMulticast_clicked(); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        switch (_id) {
+        default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+        case 0:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< QAbstractSocket::SocketState >(); break;
+            }
+            break;
+        }
+    }
 }
 
 const QMetaObject UdpMultiCast::staticMetaObject = {
@@ -84,6 +137,17 @@ void *UdpMultiCast::qt_metacast(const char *_clname)
 int UdpMultiCast::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
 {
     _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 7)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 7;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 7)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 7;
+    }
     return _id;
 }
 QT_WARNING_POP
