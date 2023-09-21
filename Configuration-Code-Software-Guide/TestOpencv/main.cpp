470c6fdcb0565bc46ea481d776f5b2d574417af0
chenbei97 <chenbei_electric@163.com>
2023-09-21 19:30:35 +0800

    u

---
+++ Configuration-Code-Software-Guide/TestOpencv/main.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/xfeatures2d.hpp>
+#include <QDir>
+#include <QDebug>
+using namespace std;
+using namespace cv;
+int main(int argc, char *argv[])
+{
+    auto dir = QDir::current();
+    dir.cdUp();
+    auto path = dir.path()+"/myBaby.jpg";
+    qDebug()<<path;
+    cout<<"hello opencv"<<endl;
+    cv::Mat src = cv::imread(path.toStdString().c_str(),IMREAD_GRAYSCALE);
+    if (!src.empty())
+    {
+        std::cout << "src.depth()=" << src.depth() << std::endl;
+        cv::namedWindow("input", WINDOW_FREERATIO);//可调的自由比例
+        cv::imshow("input", src);
+        cv::waitKey(0);
+        cv::destroyAllWindows();
+    }
+    else
+        printf("could not load image...");
+    return 0;
+}
+
